aea11f1035afacfa2b7e2cc7910f89709caf91a2
Adam Collard <adam.collard@gmail.com>
2016-06-09 21:18:49 +0100

    Pre-show... who knows?

--- findjesus.py
+++ findjesus.py
@@ -1,9 +1,14 @@
 import sys
 
 import bfs
+import signal_handeling
+
+
 
 def main():
+    signal_handeling.prepareToFindJesus()
     print(bfs.bfs("http://en.wikipedia.org/wiki/Special:Random"))
+    signal_handeling.jesusFound()
 
 
 if __name__ == '__main__':
